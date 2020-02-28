4f0e702652398e88fb4bb80bd9b0b0c8af766098
rahomohamed <rahojmohamed@gmail.com>
2020-02-28 01:51:37 +0000

    cleaned up code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
+
+// sets max number of characters a user inputs in a tweet to 140 
 $('.new-tweet textarea').on('input', function() {
 let input = $(this).val().length; // whatever you are typing in
 let maxChar = 140;
 let remainingChar = maxChar - input;
 
+// changes counter color to red when the max is reached
 $('.counter').text(remainingChar)
 if (remainingChar <= 0) {
 $('.counter').css("color", 'red')
